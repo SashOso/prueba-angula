a5e3ee1bce644a3da3e07d7af64edabe0a67c50c
andrestch <andrestuesta_22@outlook.com>
2024-12-02 17:54:00 -0500

    -

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { EntradasComponent } from './components/entradas/entradas.component';
+import { SalidasComponent } from './components/salidas/salidas.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent , pathMatch: 'full' },
     {path:'home',component:HomeComponent},
-    {path:'login',component:LoginComponent}
+    {path:'login',component:LoginComponent},
+    {path:'entradas',component:EntradasComponent},
+    {path:'salidas',component:SalidasComponent}
 ];
